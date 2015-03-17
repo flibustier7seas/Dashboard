5a1b7c21f9f4b8823967d98b411b31a37822610b
Дик Максим Евгеньевич <m.dik@2Gis.ru>
2015-03-17 12:56:56 +0600

    Добавил фильтр

--- scripts/app/viewModels/appViewModel.js
+++ scripts/app/viewModels/appViewModel.js
@@ -1,7 +1,26 @@
 define(["jquery", "ko", "./repositoryViewModel"], function ($, ko, repositoryViewModel) {
     return function (client) {
         var self = this;
+
+        this.filters = [
+            { title: 'Show All', filter: null },
+            { title: 'Only erm', filter: function (item) { return item.name() == 'erm'; } }
+        ];
+
+        this.activeFilter = ko.observable(self.filters[1].filter);
+
         this.listOfRepositories = ko.observableArray();
+
+        this.filteredListOfRepositories = ko.computed(function () {
+            var result;
+            if (self.activeFilter()) {
+                result = ko.utils.arrayFilter(self.listOfRepositories(), self.activeFilter());
+            } else {
+                result = self.listOfRepositories();
+            }
+            return result;
+        });
+
         this.sort = function () {
             self.listOfRepositories.sort(function (left, right) {
                 return left.name() == right.name() ? 0 : (left.name() < right.name() ? -1 : 1);
