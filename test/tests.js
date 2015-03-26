8c08c13d1d213a7fc8c993e7ed593e69feef5508
flibustier7seas <flibustier7seas@gmail.com>
2015-03-26 18:01:55 +0600

    Добавил папку для тестов

---
+++ test/tests.js
@@ -0,0 +1,11 @@
+﻿define(["app/viewModels/reviewerViewModel", "qunit"], function (reviewerViewModel) {
+    $(function () {
+        var a = new reviewerViewModel({
+            displayName: null,
+            vote:0
+        });
+        test("Test", function() {
+            ok(a.titleVote, "No vote");
+        });
+    });
+});
