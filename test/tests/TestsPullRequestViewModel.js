bb8c92dfff2b34f6cb38b833c36aebcbd32cdee2
flibustier7seas <flibustier7seas@gmail.com>
2015-03-27 16:14:43 +0600

    test add

---
+++ test/tests/TestsPullRequestViewModel.js
@@ -0,0 +1,52 @@
+﻿define(["app/viewModels/pullRequestViewModel"], function (pullRequestViewModel) {
+    var run = function () {
+
+        var self = this;
+
+        this.pullRequest = {
+            pullRequestId: null,
+            status: null,
+            title: null,
+            createdByDisplayName: null,
+            creationDate: null,
+            sourceRefName: null,
+            description: null,
+            url: null,
+            mergeStatus: null,
+            lastMergeSourceCommitId: null
+        };
+
+        this.repository = {
+            name: function () { },
+            url: function () { },
+            id: function () { }
+        };
+
+        this.reviewers = [
+            { displayName: null, vote: -10 },
+            { displayName: null, vote: 0 }
+        ];
+
+        this.client = {
+            getCommit: function () {
+                return {
+                    done: function () { return null; }
+                }
+            },
+            getReviewers: function () {
+                return {
+                    done: function (callback) {
+                        return callback(self.reviewers);
+                    }
+                }
+            },
+        };
+
+        var item = new pullRequestViewModel(self.pullRequest, self.repository, self.client);
+
+        test("Test pullRequestViewModell", function () {
+            ok(item.titleMinVote() == "No", "vote:-10,0;Status: 'No'");
+        });
+    }
+    return { run: run };
+});
---
+++ test/unittestsmain.js
@@ -0,0 +1,34 @@
+﻿requirejs.config({
+    "paths": {
+        "app": "../scripts/app",
+        "jquery": "../scripts/libs/jquery-2.1.0",
+        "ko": "../scripts/libs/knockout-3.3.0",
+        "moment": "../scripts/libs/moment",
+        "qUnit" : "qunit-1.17.1"
+    }
+    ,
+    shim: {
+        'qUnit': {
+            exports: 'qUnit',
+            init: function () {
+                QUnit.config.autoload = false;
+                QUnit.config.autostart = false;
+            }
+        }
+    }
+});
+
+// require the unit tests.
+require(
+['qUnit', 'tests/testsReviewerViewModel', 'tests/testsPullRequestViewModel'],
+    function (qUnit, testsReviewerViewModel, testsPullRequestViewModel) {
+
+        // run the tests.
+        testsReviewerViewModel.run();
+        testsPullRequestViewModel.run();
+
+        // start QUnit.
+        QUnit.load();
+        QUnit.start();
+    }
+);
