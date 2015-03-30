a1e464c0b82cb1945ac8de5f07650ff207705836
flibustier7seas <flibustier7seas@gmail.com>
2015-03-30 15:27:13 +0600

    Добавил factory

---
+++ scripts/app/factory.js
@@ -0,0 +1,53 @@
+﻿define(["jquery", "ko", "./models/pullRequest", "./models/commit", "./models/reviewer"],
+    function ($, ko, pullRequestModel, commit, reviewer) {
+        var API_PULLREQUEST = "/pullRequest";
+
+        return function (client) {
+            this.getRepositories = function () {
+                return client.getRepositories();
+            }
+
+            this.getPullRequests = function (repository) {
+                return client.getPullRequests(repository.id)
+                    .then(
+                    function (items) {
+                        return $.map(items.value || [], function (item) {
+
+                            var pullRequest = new pullRequestModel(
+                                item.pullRequestId,
+                                item.status,
+                                item.title,
+                                API_PULLREQUEST + '/' + item.pullRequestId,
+                                item.createdBy.displayName,
+                                item.createdBy.id,
+                                item.lastMergeSourceCommit.commitId,
+                                item.creationDate,
+                                item.sourceRefName,
+                                item.mergeStatus,
+                                item.description,
+                                repository.name,
+                                repository.url
+                            );
+
+                            client.getCommits(item.sourceRefName, item.targetRefName, item.repository.id)
+                                .then(function (data) {
+                                    data.value.forEach(function (cmt) {
+                                        pullRequest.addCommit(new commit(cmt.commitId, cmt.committer.date, cmt.comment));
+                                    });
+                                });
+
+                            client.getReviewers(item.repository.id, item.pullRequestId)
+                                .then(function (data) {
+                                    data.value.forEach(function (rvw) {
+                                        pullRequest.addReviewer(new reviewer(rvw.displayName,rvw.id, rvw.vote));
+                                    });
+
+                                });
+
+                            return pullRequest;
+                        });
+                    });
+            }
+        };
+    });
+
